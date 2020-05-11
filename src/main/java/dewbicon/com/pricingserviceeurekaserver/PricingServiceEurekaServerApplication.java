bfc2c92950812a330c1930bf18d9e47cfc36e12a
ginowine <ginowine1@gmail.com>
2020-05-11 08:52:25 +0100

    Initial commit

---
+++ src/main/java/dewbicon/com/pricingserviceeurekaserver/PricingServiceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package dewbicon.com.pricingserviceeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingServiceEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingServiceEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dewbicon/com/pricingserviceeurekaserver/PricingServiceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dewbicon.com.pricingserviceeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingServiceEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
