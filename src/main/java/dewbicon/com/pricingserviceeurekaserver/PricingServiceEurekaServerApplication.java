14973b873166b0c9df75861e55aa767554671f34
ginowine <ginowine1@gmail.com>
2020-05-11 08:56:45 +0100

    Eureka server setup

--- src/main/java/dewbicon/com/pricingserviceeurekaserver/PricingServiceEurekaServerApplication.java
+++ src/main/java/dewbicon/com/pricingserviceeurekaserver/PricingServiceEurekaServerApplication.java
@@ -2,8 +2,10 @@ package dewbicon.com.pricingserviceeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PricingServiceEurekaServerApplication {
 
 	public static void main(String[] args) {
